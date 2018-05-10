7523447f3834d6b95fdb08716ebe1e09f41ee106
Luke Alvoeiro <lalvoeiro@middlebury.edu>
2018-05-10 16:02:14 +0200

    registration works

--- app/src/main/java/mil/android/babysitter/AttachPictureActivity.java
+++ app/src/main/java/mil/android/babysitter/AttachPictureActivity.java
@@ -2,18 +2,32 @@ package mil.android.babysitter;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,7 +36,16 @@ import mil.android.babysitter.data.User;
 
 public class AttachPictureActivity extends AppCompatActivity {
 
+    public static final String blankUserUrl = "https://i.imgur.com/lNziRwJ.jpg";
+
+    private boolean imgChanged;
+    public static final int PICK_IMAGE = 1;
+
+    @BindView(R.id.imgAttach)
+    ImageView imgAttach;
+
     private ProgressDialog progressDialog;
+    private StorageReference mStorageRef;
     FirebaseAuth firebaseAuth = null;
 
     private String email;
@@ -42,12 +65,36 @@ public class AttachPictureActivity extends AppCompatActivity {
         phoneNumber = getIntent().getExtras().getString("PhoneNumber");
         babysitter = getIntent().getExtras().getBoolean("Babysitter");
 
+        imgChanged = false;
+
         ButterKnife.bind(this);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
-    @OnClick(R.id.tvFinish)
-    public void finishRegistration(){
+    @OnClick(R.id.btnAttach)
+    void attachClick(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            Uri selectedimg = data.getData();
+            try {
+                imgAttach.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedimg));
+                imgChanged = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void finishRegistration(final String imageUrl){
         showProgressDialog();
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
@@ -64,6 +111,8 @@ public class AttachPictureActivity extends AppCompatActivity {
                     String key = FirebaseDatabase.getInstance().getReference().child("user").push().getKey();
                     User newUser = new User(userId, name, email, phoneNumber, babysitter);
 
+                    newUser.setImageUrl(imageUrl);
+
                     FirebaseDatabase.getInstance().getReference().child("user").child(key).setValue(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
@@ -78,6 +127,23 @@ public class AttachPictureActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @OnClick(R.id.tvFinish)
+    void sendClick() {
+        if (!imgChanged) {
+            try {
+                finishRegistration(blankUserUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                uploadUserWithImage();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void showProgressDialog() {
@@ -101,6 +167,38 @@ public class AttachPictureActivity extends AppCompatActivity {
         } else {
             return email;
         }
+    }
+
+    public void uploadUserWithImage() throws Exception {
+        imgAttach.setDrawingCacheEnabled(true);
+        imgAttach.buildDrawingCache();
+        Bitmap bitmap = imgAttach.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageInBytes = baos.toByteArray();
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        String newImage = URLEncoder.encode(UUID.randomUUID().toString(), "UTF-8")+".jpg";
+        StorageReference newImageRef = storageRef.child(newImage);
+        StorageReference newImageImagesRef = storageRef.child("images/"+newImage);
+        newImageRef.getName().equals(newImageImagesRef.getName());    // true
+        newImageRef.getPath().equals(newImageImagesRef.getPath());    // false
+
+        UploadTask uploadTask = newImageImagesRef.putBytes(imageInBytes);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toast.makeText(AttachPictureActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                String imgUrlToRegister = taskSnapshot.getDownloadUrl().toString();
+                finishRegistration(imgUrlToRegister);
+            }
+        });
     }
 }
 
